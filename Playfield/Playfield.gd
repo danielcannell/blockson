ac251e8fffabf801454b611a01d1393be376cc07
Jamie Wood <jamie.wood70@ntlworld.com>
2019-04-27 15:04:16 +0100

    Set map size in config

--- Playfield/Playfield.gd
+++ Playfield/Playfield.gd
@@ -4,11 +4,21 @@ signal end_placing
 signal balance_changed
 
 onready var tilemap = find_node("TileMap")
+onready var background = find_node("Background")
 
 var balance = 0
 
 var machines = []
 var tiles = {}
+
+
+func _ready():
+    var tx = tilemap.get_cell_size().x
+    var ty = tilemap.get_cell_size().y
+    var mapsize = Vector2(Config.MAP_WIDTH * tx, Config.MAP_HEIGHT * ty)
+    background.set_size(mapsize)
+    background.set_position(-mapsize / 2)
+    tilemap.set_position(-mapsize / 2)
 
 
 func get_tile_coord(viewport_pos):
@@ -22,10 +32,12 @@ func _unhandled_input(event):
 
     if event is InputEventMouseButton:
         if not event.is_pressed():
-            print("release")
-            # Place or select building
+            if tilepos.x >= 0 and tilepos.y >= 0 and tilepos.x < Config.MAP_WIDTH and tilepos.y < Config.MAP_HEIGHT:
+                # Place or select building
+                tilemap.set_cell(tilepos.x, tilepos.y, 2)
     elif event is InputEventMouseMotion:
-        #print("mouse motion at: ", event.position)
+        # Render rect on screen?
+        #print("mouse motion at: ", tilepos)
         pass
 
 
