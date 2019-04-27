f97e4aab35c4b94a7e454b6e7ffab6a24dbd76c4
oddeyed <ben.janoff@gmail.com>
2019-04-27 18:02:42 +0100

    Disable tutorial by default.

--- Tutorial/Tutorial.gd
+++ Tutorial/Tutorial.gd
@@ -4,7 +4,7 @@ const events = Globals.TutorialEvents
 
 # If active is false, then the tutorial will not run.
 # If active is true, then the tutorial will run. Simples!
-var active = true
+var active = false
 var occured = []
 onready var popup = get_node("Popup")
 onready var instructions = get_node("Popup/Panel/VBoxContainer/RichTextLabel")
@@ -33,15 +33,15 @@ My thoughts are recorded in a Merkle Tree, and a cryptocurrency is my life.
 
 I need your help to improve my datacentre so I can keep the price of Bitcoin high.
 That way people will keep mining and I get to stay alive."""
-    popup.visible = true    
+    popup.visible = true
 
 
 func handle_playfield_ready():
     get_tree().paused = true
     instructions.text = """Blah blah balh."""
-    popup.visible = true    
-    
-    
+    popup.visible = true
+
+
 func handle_popup_ok_pressed():
     get_tree().paused = false
     get_node("Popup").visible = false
