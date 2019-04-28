dbe9f4fbfa96117ac2b7c066b346d4fd5d1ceb50
Daniel Cannell <danielcannell@googlemail.com>
2019-04-28 20:00:47 +0100

    Fix merge

--- TechController.gd
+++ TechController.gd
@@ -18,10 +18,6 @@ func _init():
             tech_states[name] = Globals.TechState.new(1.0, true)
         else:
             tech_states[name] = Globals.TechState.new(0.0, false)
-            
-
-func _ready():
-    emit_signal("tech_update", tech_states)
 
 
 func _ready():
