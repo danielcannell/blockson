f58fd5f5577c94ff77ce4f836f7823e7ef98a09b
oddeyed <ben.janoff@gmail.com>
2019-04-29 19:08:15 +0100

    Add level progression

--- Economy.gd
+++ Economy.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 signal balance_updated
+signal level_completed
+signal player_win
 
 
 var income_per_sec = 0.0
@@ -13,6 +15,13 @@ var btc_balance = 100000.0
 func _process(delta):
     btc_balance += delta * income_per_sec
     emit_signal("balance_updated", btc_balance, thoughts_per_sec)
+    
+    if thoughts_per_sec > Config.LEVEL_THRESHOLDS[Config.level]:
+        if Config.level == len(Config.LEVEL_THRESHOLDS) - 1:
+            emit_signal("player_win")
+            return
+        emit_signal("level_completed")
+
 
 
 func on_playfield_mining_result(_thoughts_per_sec, _income_per_sec):
