04579dfa07b9ee4a0b7b809a6fe5bd1fd0a79fbf
Jamie Wood <jamie.wood70@ntlworld.com>
2019-04-29 18:45:55 +0100

    Add tutorial popups for net issues

--- Tutorial/Tutorial.gd
+++ Tutorial/Tutorial.gd
@@ -6,6 +6,8 @@ onready var instructions = get_node("Popup/Panel/VBoxContainer/RichTextLabel")
 var handlers = {
     Globals.TutorialEvents.UI_READY: "handle_ui_ready",
     Globals.TutorialEvents.PLAYFIELD_READY: null,
+    Globals.TutorialEvents.NET_FANOUT_TOO_HIGH: "handle_net_fanout_too_high",
+    Globals.TutorialEvents.NET_OVERLOADED: "handle_net_overloaded",
 }
 
 
@@ -42,6 +44,29 @@ That way people will keep mining and I get to stay alive."""
 func handle_playfield_ready():
     get_tree().paused = true
     instructions.text = """Blah blah balh."""
+    popup.visible = true
+
+
+func handle_net_fanout_too_high():
+    get_tree().paused = true
+    instructions.text = """You've connected too many machines to a single network.
+
+Blue data networks have a maximum number of devices which can be connected to each other on a single wire.
+If you connect too many devices, none of them will work.
+
+The network ports in the wall have a limit of 1 device.
+A Router can be used to allow more connections: 4 devices can be connected to its lower port.
+
+Try placing a Router to solve the overload."""
+    popup.visible = true
+
+
+func handle_net_overloaded():
+    get_tree().paused = true
+    instructions.text = """You're drawing too much power from a single power transformer.
+
+Transformers can supply a limited amount of power.
+You can connect the output of several transformers together to provide more power."""
     popup.visible = true
 
 
